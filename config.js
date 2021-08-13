54a682c1a27e17e83271e36e8fa9d34d6616b0e7
Nguyen Van Chien <88893226+Chienchuabietiu@users.noreply.github.com>
2021-08-14 06:38:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Babe Chien co dep trai khong  ',
-    btnNo: 'Xấu như chó ',
+    btnNo: 'Xấu như chó nek ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại anh đẹp trai hêhe ',
